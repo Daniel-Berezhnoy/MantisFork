6b62bb9726350fb7a6c713dbe316fa360fc17bbd
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2025-02-12 12:39:20 -0800

    Trying this

--- Sources/Mantis/Mantis.swift
+++ Sources/Mantis/Mantis.swift
@@ -112,7 +112,7 @@ private func buildCropView(withImage image: UIImage,
     
     cropView.setFixedRatio(
         16 / 7,
-        zoom: true,
+//        zoom: true,
         presetFixedRatioType: .alwaysUsingOnePresetFixedRatio(ratio: 16/7)
     )
     
