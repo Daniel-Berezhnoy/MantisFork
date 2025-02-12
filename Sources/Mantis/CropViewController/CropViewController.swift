24553661f9369c77af32f9e9eab6e5992694444a
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2025-02-12 12:13:07 -0800

    Test

--- Sources/Mantis/CropViewController/CropViewController.swift
+++ Sources/Mantis/CropViewController/CropViewController.swift
@@ -128,7 +128,7 @@ open class CropViewController: UIViewController {
             case .canUseMultiplePresetFixedRatio(let defaultRatio):
                 if defaultRatio > 0 {
                     setFixedRatio(defaultRatio)
-                    cropView.aspectRatioLockEnabled = true
+//                    cropView.aspectRatioLockEnabled = true
                     config.cropToolbarConfig.presetRatiosButtonSelected = true
                 }
                 
